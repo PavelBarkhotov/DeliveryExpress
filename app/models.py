f16e1817ecedaeb0f19d219047cee4ccf48aa26d
pavelbarhotov <pavelbarhotov@yandex.ru>
2026-08-15 14:09:06 +0300

    Добавлен gitignore и дописаны модели и схемы

---
+++ app/models.py
@@ -0,0 +1,31 @@
+from decimal import Decimal
+
+from sqlalchemy import ForeignKey, Index, DECIMAL
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+
+class Base(DeclarativeBase):
+    pass
+
+class ParcelType(Base):
+    __tablename__ = "parcel_types"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+
+class Parcel(Base):
+    __tablename__ = "parcels"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    weight: Mapped[Decimal] = mapped_column(DECIMAL(6, 2))
+    type_id: Mapped[int] = mapped_column(ForeignKey(ParcelType.id))
+    parcel_type: Mapped["ParcelType"] = relationship()
+    dollar_price: Mapped[Decimal] = mapped_column(DECIMAL(8, 2))
+    delivery_price: Mapped[Decimal | None] = mapped_column(DECIMAL(10, 2), nullable=True)
+    user_session: Mapped[str]
+
+    __table_args__ = (
+        Index("idx_session_type", "user_session", "type_id"),
+    )
+
---
+++ app/schemas.py
@@ -0,0 +1,35 @@
+from decimal import Decimal
+
+from pydantic import BaseModel, field_validator, Field, ConfigDict
+
+class ParcelTypeResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int = Field(gt=0)
+    name: str = Field(max_length=64)
+
+class ParcelRequest(BaseModel):
+    name: str = Field(max_length=64)
+    weight: Decimal = Field(gt=0, max_digits=6, decimal_places=2)
+    type_id: int = Field(gt=0)
+    dollar_price: Decimal = Field(gt=0, max_digits=8, decimal_places=2)
+
+    @field_validator("name")
+    @classmethod
+    def name_not_empty(cls, v: str) -> str:
+        v = v.strip()
+        if not v:
+            raise ValueError("Имя посылки не может быть пустым")
+        return v
+
+class ParcelResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    name: str = Field(max_length=64)
+    weight: Decimal = Field(gt=0, max_digits=6, decimal_places=2)
+    parcel_type: ParcelTypeResponse
+    dollar_price: Decimal = Field(gt=0, max_digits=8, decimal_places=2)
+    delivery_price: Decimal | None = Field(default=None, gt=0, max_digits=10, decimal_places=2)
+
+
